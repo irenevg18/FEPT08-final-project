9704be9334afacd1b26a422fe44d2f467d47877b
irenevg18 <irenevalleguillen@gmail.com>
2024-12-03 10:33:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="text-8xl font-black text-emerald-800">{{title}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data(){
+    return{
+      title: "Holaaa!🪲"
+    }
+  }
+  
+}
+</script>
+<style>
+  
+</style>
